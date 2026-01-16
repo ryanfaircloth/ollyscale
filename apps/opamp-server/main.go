e7cdc380dfd5b38a369847b16dc1af95a6eb58c9
Ryan Faircloth <ryan@dss-i.com>
2026-01-16 15:24:50 -0600

    fix(opamp-server): reset version

--- apps/opamp-server/main.go
+++ apps/opamp-server/main.go
@@ -58,7 +58,7 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
-var version = "1.0.0"
+var version = "0.0.0"
 var tracer trace.Tracer
 
 // AgentState tracks the state of a connected OTel Collector agent
